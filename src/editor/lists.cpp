2727a616172be13f048d114acea3bfbf70d463cb
peachey2k2 <ogzpolat@gmail.com>
2024-06-07 19:33:54 +0300

    reshape file lists

--- src/editor/lists.cpp
+++ src/editor/lists.cpp
@@ -1,5 +1,7 @@
 #include "editor/lists.h"
 
+#define OUTER_MARGIN 8
+
 using namespace godot;
 
 void VMTList::_bind_methods() {
@@ -8,6 +10,39 @@ void VMTList::_bind_methods() {
 VMTList::VMTList() {
     set_h_size_flags(SIZE_EXPAND_FILL);
     set_v_size_flags(SIZE_EXPAND_FILL);
+
+    MarginContainer *margin = memnew(MarginContainer);
+    margin->set_anchors_and_offsets_preset(Control::PRESET_FULL_RECT);
+    margin->begin_bulk_theme_override();
+    margin->add_theme_constant_override("margin_left", OUTER_MARGIN);
+    margin->add_theme_constant_override("margin_top", OUTER_MARGIN);
+    margin->add_theme_constant_override("margin_right", OUTER_MARGIN);
+    margin->add_theme_constant_override("margin_bottom", OUTER_MARGIN);
+    margin->end_bulk_theme_override();
+    add_child(margin);
+
+    VBoxContainer *vbox = memnew(VBoxContainer);
+    vbox->set_anchors_and_offsets_preset(Control::PRESET_FULL_RECT);
+    margin->add_child(vbox);
+
+
+    HBoxContainer *hotbar = memnew(HBoxContainer);
+    hotbar->set_h_size_flags(SIZE_EXPAND_FILL);
+    ADD_BUTTON(hotbar, New, "ScriptCreate");
+    ADD_BUTTON(hotbar, Remove, "ScriptRemove");
+    title = memnew(Label);
+    title->set_horizontal_alignment(HorizontalAlignment::HORIZONTAL_ALIGNMENT_RIGHT);
+    title->set_h_size_flags(SIZE_EXPAND_FILL);
+    hotbar->add_child(title);
+    vbox->add_child(hotbar);
+
+    HBoxContainer *search = memnew(HBoxContainer);
+    search->set_h_size_flags(SIZE_EXPAND_FILL);
+    LineEdit *searchbar = memnew(LineEdit);
+    searchbar->set_h_size_flags(SIZE_EXPAND_FILL);
+    search->add_child(searchbar);
+    ADD_BUTTON(search, Search, "Search");
+    vbox->add_child(search);
 
     list = memnew(ItemList);
     list->set_h_size_flags(SIZE_EXPAND_FILL);
@@ -22,7 +57,7 @@ VMTList::VMTList() {
     list->add_item("Item 8");
     list->add_item("Item 9");
     list->add_item("Item 10");
-    add_child(list);
+    vbox->add_child(list);
 }
 
 VMTList::~VMTList() {
@@ -32,6 +67,7 @@ void VMTScriptList::_bind_methods() {
 }
 
 VMTScriptList::VMTScriptList() {
+    set_title("Scripts");
 }
 
 VMTScriptList::~VMTScriptList() {
@@ -41,6 +77,7 @@ void VMTFilterList::_bind_methods() {
 }
 
 VMTFilterList::VMTFilterList() {
+    set_title("Filters");
 }
 
 VMTFilterList::~VMTFilterList() {
--- src/editor/lists.h
+++ src/editor/lists.h
@@ -1,18 +1,37 @@
 #pragma once
 
+#include <godot_cpp/classes/panel.hpp>
+#include <godot_cpp/classes/margin_container.hpp>
 #include <godot_cpp/classes/v_box_container.hpp>
+#include <godot_cpp/classes/h_box_container.hpp>
+#include <godot_cpp/classes/label.hpp>
 #include <godot_cpp/classes/item_list.hpp>
+#include <godot_cpp/classes/button.hpp>
+#include <godot_cpp/classes/line_edit.hpp>
+#include <godot_cpp/classes/editor_interface.hpp>
+
+#define ADD_BUTTON(m_parent, m_name, m_icon)                                                            \
+    Button *m_name = memnew(Button);                                                                    \
+    m_name->add_theme_icon_override(                                                                    \
+        "icon",                                                                                         \
+        EditorInterface::get_singleton()->get_base_control()->get_theme_icon(m_icon, "EditorIcons")     \
+    );                                                                                                  \
+    m_name->set_tooltip_text(m_name->get_name());                                                       \
+    m_parent->add_child(m_name);                                                                        \
 
 namespace godot {
 
-class VMTList : public VBoxContainer {
-    GDCLASS(VMTList, VBoxContainer);
+class VMTList : public Panel {
+    GDCLASS(VMTList, Panel);
 
     private:
         ItemList *list;
+        Label *title;
 
     protected:
         static void _bind_methods();
+
+        void set_title(const String &p_title) { title->set_text(p_title); }
 
     public:
         VMTList();
